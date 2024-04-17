a579ec1b17a61a6f92eb5eb27ea2ac7becc25342
LENOVO <LENOVO@Awais-Office-Laptop>
2024-04-17 16:39:39 +0500

    Aman export Test case

--- selenium_jenkins/src/main/java/selenium_jenkins_testcases/Efx_AmanExport.java
+++ selenium_jenkins/src/main/java/selenium_jenkins_testcases/Efx_AmanExport.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class amanexport {
+public class Efx_AmanExport {
 	public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
